92b9d20b60393d385c95a0a779cc493d8c16bc0e
PlayerLi <615961877@qq.com>
2022-10-09 14:13:14 +0800

    first git

--- src/main/java/com/itheima/git/GitTest.java
+++ src/main/java/com/itheima/git/GitTest.java
@@ -3,5 +3,6 @@ package com.itheima.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("first git");
+        System.out.println("second git");
     }
 }
